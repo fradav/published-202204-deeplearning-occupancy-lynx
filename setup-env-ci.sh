677f56e0f46fbc43a9e7b1259b654142a98aa7fc
François-David Collin <Francois-David.Collin@umontpellier.fr>
2024-10-17 10:03:29 +0200

    last CI try before switch to singularity

---
+++ setup-env-ci.sh
@@ -0,0 +1,2 @@
+sudo apt-get -y install libcurl4-openssl-dev libudunits2-dev libgdal-dev
+R -e "fastai::install_fastai()"
---
+++ setup-render-ci.sh
@@ -0,0 +1,1 @@
+sudo apt-get -y install libcurl4-openssl-dev libudunits2-dev libgdal-dev
